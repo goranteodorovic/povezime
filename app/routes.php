50ebec9a9e21a4b157ea818cb80b7d57411d967c
goranteodorovic <fb.goran@gmail.com>
2017-10-30 17:23:25 +0100

    Add files via upload

--- app/routes.php
+++ app/routes.php
@@ -16,11 +16,13 @@ $app->post('/car/delete', 'CarController:delete');
 /*-----------------------------------------------------*/
 /* Ride Offers */
 $app->post('/ride/offer', 'OfferController:rideOffer');
+$app->post('/ride/offer/update', 'OfferController:updateOffer');
 //$app->post('/ride/offer/cancel', 'OfferController:cancelOffer');
 
 /*-----------------------------------------------------*/
 /* Ride Searches */
 $app->post('/ride/search', 'SearchController:rideSearch');
+//$app->post('/ride/search/update', 'SearchController:updateSearch');
 //$app->post('/ride/search/cancel', 'SearchController:cancelSearch');
 
 /*-----------------------------------------------------*/
@@ -30,5 +32,5 @@ $app->post('/ride/request/cancel', 'RideRequestController:cancelRequest');
 
 $app->post('/offer/ride/request', 'RideRequestController:offerRideRequest');
 $app->post('/offer/ride/answer', 'RideRequestController:offerRideAnswer');
-$app->post('/search/ride/request', 'RideRequestController:searchRideRequest');
-$app->post('/search/ride/answer', 'RideRequestController:searchRideAnswer');
+//$app->post('/search/ride/request', 'RideRequestController:searchRideRequest');
+//$app->post('/search/ride/answer', 'RideRequestController:searchRideAnswer');
